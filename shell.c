2d8ec29dc79b05b2df74f2dceff1e9b1e044be74
Luciano Barletta <lushobarlett@gmail.com>
2022-03-31 14:38:54 -0300

    refactor shell

--- shell.c
+++ shell.c
@@ -3,47 +3,76 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <stdbool.h>
 
 #define MAX_LINE 100
 #define MAX_ARGS 10
 
+bool is_exit_command(const char* buf) {
+	return strcmp(buf, "exit") == 0;
+}
+
+void prompt() {
+	printf(">> ");
+}
+
+void read_command(char* buf, char** args) {
+	fgets(buf, MAX_LINE, stdin);
+	buf[strcspn(buf, "\r\n")] = 0;
+
+	char* token = strtok(buf, " ");
+	int i = 0;
+
+	while(token != NULL) {
+		args[i++] = token;
+		token = strtok(NULL, " ");
+	}
+
+	args[i] = NULL;
+}
+
+bool fork_failed(pid_t process_id) {
+	return process_id == -1;
+}
+
+bool is_parent_process(pid_t process_id) {
+	return process_id > 0;
+}
+
+void execute_command(const char* buf, char * const* args) {
+	pid_t process_id = fork();
+
+	if (fork_failed(process_id)) {
+		printf("Shell error.\n");
+		return;
+	}
+
+	if (is_parent_process(process_id)) {
+		wait(NULL);
+		return;
+	}
+
+	execv(buf, args);
+	printf("Error executing the command.\n");
+	exit(0);
+}
+
 int main() {
 
 	while(1) {
-		char buf[MAX_LINE] = "\0";
+		char buf[MAX_LINE];
 		char* args[MAX_ARGS];
 
-		printf(">> ");
+		prompt();
 
-		fgets(buf, MAX_LINE, stdin);
-		buf[strcspn(buf, "\r\n")] = 0;
-
-		char* token = strtok(buf, " ");
-		int i = 0;
-
-		while(token != NULL) {
-			args[i++] = token;
-			token = strtok(NULL, " ");
-		}
-
-		args[i] = NULL;
+		read_command(buf, args);
 		
-		if (strcmp(buf, "exit")) {
+		if (is_exit_command(buf)) {
 			printf("Exiting the console.\n");
 			break;
 		}
 
-		pid_t pid = fork();
-
-		if (pid == 0){
-			execv(buf, args);
-			printf("Error executing the command.\n");
-			exit(0);
-		} else if (pid > 0) {
-			wait(NULL);
-		} else {
-			printf("Shell error.\n");
-		}
+		execute_command(buf, args);
 	}
 
 	return 0;
