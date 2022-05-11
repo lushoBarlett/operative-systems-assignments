0b47ec12d1df8fe368830b46bcaea36431c7c1a8
Luciano <lushobarlett@gmail.com>
2022-05-11 00:45:21 -0300

    correct header types

--- P2/block.h
+++ P2/block.h
@@ -13,10 +13,10 @@ struct block_t {
 
 typedef struct block_t block_t;
 
-void block_init(block_t* block);
+void block_init(block_t* block, unsigned int counter);
 
 void block_enter(block_t* block);
 
 void block_leave(block_t* block);
 
-void block_wait(block_t* block);
+void block_wait(block_t* block, pthread_mutex_lock_t* lock);
