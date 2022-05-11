514430bbc23bc78a3420234f878a74ca0dbcde1a
Luciano <lushobarlett@gmail.com>
2022-05-11 02:04:07 -0300

    extend the functionality of block

--- P2/block.c
+++ P2/block.c
@@ -15,17 +15,39 @@ void block_enter(block_t* block) {
 
 }
 
-void block_leave(block_t* block) {
+void block_leave(block_t* block, int broadcast) {
 	pthread_mutex_lock(&block->counter_lock);
 
 	block->counter--;
 
-	if (block->counter == 0)
-		pthread_cond_broadcast(&block->depleted);
+	if (block->counter == 0) {
+	 	if (broadcast)
+			pthread_cond_broadcast(&block->depleted);
+		else
+			pthread_cond_signal(&block->depleted);
+	}
 
 	pthread_mutex_unlock(&block->counter_lock);
 }
 
+void block_leave_signal(block_t* block) {
+	block_leave(block, 0);
+}
+
+void block_leave_broadcast(block_t* block) {
+	block_leave(block, 1);
+}
+
 void block_wait(block_t* block, pthread_mutex_t* lock) {
 	pthread_cond_wait(&block->depleted, lock);
+}
+
+unsigned int block_counter(block_t* block) {
+	pthread_mutex_lock(&block->counter_lock);
+
+	int counter = block->counter;
+
+	pthread_mutex_unlock(&block->counter_lock);
+
+	return counter;
 }
--- P2/block.h
+++ P2/block.h
@@ -17,6 +17,10 @@ void block_init(block_t* block, unsigned int counter);
 
 void block_enter(block_t* block);
 
-void block_leave(block_t* block);
+void block_leave_signal(block_t* block);
 
-void block_wait(block_t* block, pthread_mutex_lock_t* lock);
+void block_leave_broadcast(block_t* block);
+
+void block_wait(block_t* block, pthread_mutex_t* lock);
+
+unsigned int block_counter(block_t* block);
