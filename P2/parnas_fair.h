ff409f9c81e5812fe2a44436f0a2289cf373bee5
Tomás Santucci <tomisantucci@gmail.com>
2022-05-10 22:29:50 -0300

    completed initial implementation of parnas fair

--- P2/parnas_fair.h
+++ P2/parnas_fair.h
@@ -1,17 +1,20 @@
+#pragma once
+
 #include "concurrent_queue.h"
 
 #include <pthread.h>
 #include <semaphore.h>
 #include <unistd.h>
-#include <stdio.h>
 
 #define M 5
 #define N 5
 #define ARRLEN 10240
 
-int arr[ARRLEN];
+int arr[ARRLEN], reading = 0;
 concurrent_queue_t cqueue;
 sem_t writer_out;
+pthread_mutex_t reading_lk;
+pthread_cond_t last_reader_leaves;
 
 enum action_type {
 	action_read,
@@ -48,88 +51,70 @@ void action_free(action_t* action) {
 	free(action);
 }
 
+void wait_for_readers_to_leave() {
+	pthread_mutex_lock(&reading_lk);
+	while (reading > 0)
+		pthread_cond_wait(&last_reader_leaves, &reading_lk);
+	pthread_mutex_unlock(&reading_lk);
+}
+
 void* queue_master(void* arg) {
 	while(1) {
 		action_t* next = cdequeue(&cqueue);
 
 		if (next->type == action_write) {
 			sem_wait(&writer_out);
-			// TODO: wait for all readers out
+			wait_for_readers_to_leave();
 			sem_post(next->wake_up);
 		} else {
 			sem_wait(&writer_out);
 			sem_post(next->wake_up);
-			sem_post(&writer_out);
 		}
 	}
 }
 
-void* escritor(void* arg) {
-	int num = arg - (void*)0;
-
-	while (1) {
-		sleep(random() % 3);
-
-		action_t* write_action = new_write_action();
-		cenqueue(&cqueue, write_action);
-		sem_wait(write_action->wake_up);
-
-		printf("Escritor %d escribiendo\n", num);
-
-		for (int i = 0; i < ARRLEN; i++)
-			arr[i] = num;
-
-		action_free(write_action);
-		sem_post(&writer_out);
-	}
-
-	return NULL;
+action_t* write_lock() {
+	action_t* write_action = new_write_action();
+	cenqueue(&cqueue, write_action);
+	sem_wait(write_action->wake_up);
+	return write_action;
 }
 
-void* lector(void *arg) {
-	int num = arg - (void*)0;
-
-	while (1) {
-		sleep(random() % 3);
-
-		action_t* read_action = new_read_action();
-		cenqueue(&cqueue, read_action);
-		sem_wait(read_action->wake_up);
-
-		int v = arr[0];
-		int i;
-		for (i = 1; i < ARRLEN; i++)
-			if (arr[i] != v)
-				break;
-
-		if (i < ARRLEN)
-			printf("Lector %d, error de lectura\n", num);
-		else
-			printf("Lector %d, dato %d\n", num, v);
-
-		action_free(read_action);
-	}
-
-	return NULL;
+void write_unlock(action_t *write_action) {
+	action_free(write_action);
+	sem_post(&writer_out);
 }
 
-int main() {
+void add_reader() {
+	pthread_mutex_lock(&reading_lk);
+	reading++;
+	pthread_mutex_unlock(&reading_lk);
+}
+
+void leave() {
+	pthread_mutex_lock(&reading_lk);
+	if (--reading == 0)
+		pthread_cond_signal(&last_reader_leaves);
+	pthread_mutex_unlock(&reading_lk);
+}
+
+action_t* read_lock() {
+	action_t* read_action = new_read_action();
+	cenqueue(&cqueue, read_action);
+	sem_wait(read_action->wake_up);
+	add_reader();
+	sem_post(&writer_out);
+	return read_action;
+}
+
+void read_unlock(action_t* read_action) {
+	action_free(read_action);
+	leave();
+}
+
+void read_write_lock_init() {
 	pthread_t master;
-	pthread_t lectores[M], escritores[N];
-
 	concurrent_queue_init(&cqueue);
-
 	pthread_create(&master, NULL, queue_master, NULL);
-	
 	sem_init(&writer_out, 0, 1);
-
-	for (int i = 0; i < M; i++)
-		pthread_create(&lectores[i], NULL, lector, i + (void*)0);
-
-	for (int i = 0; i < N; i++)
-		pthread_create(&escritores[i], NULL, escritor, i + (void*)0);
-
-	pthread_join(lectores[0], NULL);
-
-	return 0;
 }
