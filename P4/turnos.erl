4fcd883bb5421167bf9324002f70a41a99dbbda5
Tomás Santucci <tomisantucci@gmail.com>
2022-06-08 19:39:37 -0300

    server set to passive

--- P4/turnos.erl
+++ P4/turnos.erl
@@ -16,23 +16,30 @@ receive_id_from(Counter) ->
 	end.
 
 server() ->
-	{ok, ListenSocket} = gen_tcp:listen(8000, [{reuseaddr, true}]),
+	{ok, ListenSocket} = gen_tcp:listen(8000, [{reuseaddr, true}, {active, false}]),
 	Counter = spawn(fun() -> counter(0) end),
 	wait_connect(ListenSocket, Counter).
 
 wait_connect(ListenSocket, Counter) ->
 	{ok, Socket} = gen_tcp:accept(ListenSocket),
 	spawn(fun() -> wait_connect(ListenSocket, Counter) end),
-	get_request(Socket, Counter).
+	get_request(Socket, Counter),
+	ok.
 
 get_request(Socket, Counter) ->
 	io:fwrite("Esperando mensajes de ~p~n", [Socket]),
-	receive
-		{tcp, Client, "NUEVO\n"} ->
-			Id = receive_id_from(Counter),
-			StringId = integer_to_list(Id),
-			gen_tcp:send(Client, StringId),
-			get_request(Socket, Counter);
-		{tcp, _Client, "CHAU\n"} ->
-			gen_tcp:shutdown(Socket, read_write)
+	case gen_tcp:recv(Socket, 0) of
+		{ok, "NUEVO\n"} -> io:fwrite("received: NUEVO\n~n"),
+				Id = receive_id_from(Counter),
+				StringId = integer_to_list(Id),
+				gen_tcp:send(Socket, StringId),
+				get_request(Socket, Counter);
+		{ok, "CHAU\n"} ->
+			io:fwrite("Closing a socket~n"),
+			gen_tcp:shutdown(Socket, read_write);
+		{error, closed} ->
+			io:fwrite("Closing a socket~n"),
+			gen_tcp:shutdown(Socket, read_write);
+		{error, Reason} ->
+			io:fwrite("an error occurred~s~n", [Reason])
 	end.
