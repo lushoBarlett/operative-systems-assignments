dd61c2f0941607e78581e570caa533fd69403d1a
Tomás Santucci <tomisantucci@gmail.com>
2022-06-09 12:50:41 -0300

    support for borderless messages

--- P4/turnos.erl
+++ P4/turnos.erl
@@ -2,6 +2,22 @@
 
 -export([server/0]).
 
+firstOccurrence(Rec) ->
+	N = string:str(Rec,"NUEVO\n"),
+	C = string:str(Rec,"CHAU\n"),
+	if
+	    (N > 0) and (C == 0) -> {nuevo,N};
+	    (N == 0) and (C > 0) -> chau;
+	    (N == 0) and (C == 0) -> nada;
+	    true -> if
+			N < C -> {nuevo,N};
+			true -> chau
+		    end
+	end.
+
+cut(Rec, N, Word) ->
+	string:slice(Rec,N + string:length(Word) - 1).
+
 counter(N) ->
 	receive
 		Receiver ->
@@ -23,23 +39,33 @@ server() ->
 wait_connect(ListenSocket, Counter) ->
 	{ok, Socket} = gen_tcp:accept(ListenSocket),
 	spawn(fun() -> wait_connect(ListenSocket, Counter) end),
-	get_request(Socket, Counter),
+	get_request(Socket, Counter, ""),
 	ok.
 
-get_request(Socket, Counter) ->
+get_request(Socket, Counter, Partial) ->
 	io:fwrite("Esperando mensajes de ~p~n", [Socket]),
 	case gen_tcp:recv(Socket, 0) of
-		{ok, "NUEVO\n"} -> io:fwrite("received: NUEVO\n~n"),
-				Id = receive_id_from(Counter),
-				StringId = integer_to_list(Id),
-				gen_tcp:send(Socket, StringId),
-				get_request(Socket, Counter);
-		{ok, "CHAU\n"} ->
-			io:fwrite("Closing a socket~n"),
-			gen_tcp:shutdown(Socket, read_write);
+		{ok, Packet} ->
+			String = (string:concat(Partial,Packet)),
+			decode(Socket, String, Counter);
 		{error, closed} ->
-			io:fwrite("Closing a socket~n"),
 			gen_tcp:shutdown(Socket, read_write);
 		{error, Reason} ->
 			io:fwrite("an error occurred~s~n", [Reason])
 	end.
+
+decode(Socket, Rec, Counter) ->
+	case firstOccurrence(Rec) of
+		{nuevo, N} -> 
+			sendCounter(Counter, Socket),
+			Remaining = cut(Rec, N, "NUEVO\n"),
+			decode(Socket, Remaining, Counter);
+		chau ->
+			gen_tcp:shutdown(Socket, read_write);
+		nada -> get_request(Socket, Counter, Rec)
+	end.
+
+sendCounter(Counter, Socket) ->
+	Id = receive_id_from(Counter),
+	StringId = integer_to_list(Id),
+	gen_tcp:send(Socket, StringId).
