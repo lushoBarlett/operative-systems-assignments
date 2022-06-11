90b7150168aecbc3453ae2796d49c0b59e8eb30c
Tomás Santucci <tomisantucci@gmail.com>
2022-06-11 19:13:36 -0300

    registered broadcaster PID and easier to send messages

--- P4/broadcaster.erl
+++ P4/broadcaster.erl
@@ -1,13 +1,13 @@
 -module(broadcaster).
 
--export([start/0, stop/1, spawn_clients/2, message/2, broadcaster/1, client/1]).
+-export([start/0, stop/0, spawn_clients/1, message/1, client/0, broadcaster/1]).
 
 broadcaster(Clients) ->
 	receive
 		{subscribe, Client} ->
 			broadcaster([Client | Clients]);
 		{message, Message} ->
-			lists:foreach(fun(Client) -> Client ! Message end, Clients),
+			lists:foreach(fun(Client) -> Client ! {normal, Message} end, Clients),
 			broadcaster(Clients);
 		{unsubscribe, Client} ->
 			NewList = lists:filter(fun(C) -> C /= Client end, Clients),
@@ -16,37 +16,39 @@ broadcaster(Clients) ->
 			ok
 	end.
 
-client_subscribe(Broadcaster) ->
-	Broadcaster ! {subscribe, self()}.
+client_subscribe() ->
+	broadcaster ! {subscribe, self()}.
 
-client_unsubscribe(Broadcaster) ->
-	Broadcaster ! {unsubscribe, self()}.
+client_unsubscribe() ->
+	broadcaster ! {unsubscribe, self()}.
 
-message(Broadcaster, Message) ->
-	Broadcaster ! {message, Message}.
+message(Message) ->
+	broadcaster ! {message, Message}.
 
 client_receive_message() ->
 	receive
 		{normal, Body} ->
 			io:fwrite("Recieved, ~s~n", [Body]),
-			client_receive_message();
-		{quit} ->
-			ok
+			client_receive_message()
 	end.
 
-client(Broadcaster) ->
-	client_subscribe(Broadcaster),
+client() ->
+	client_subscribe(),
 	client_receive_message(),
-	client_unsubscribe(Broadcaster).
+	client_unsubscribe().
 
-spawn_clients(_Broadcaster, 0) ->
+spawn_clients(0) ->
 	ok;
-spawn_clients(Broadcaster, N) ->
-	spawn(?MODULE, client, [Broadcaster]),
-	spawn_clients(Broadcaster, N - 1).
+spawn_clients(N) ->
+	spawn(?MODULE, client, []),
+	spawn_clients(N - 1).
 
 start() ->
-	spawn(?MODULE, broadcaster, [[]]).
+	Broadcaster = spawn(?MODULE, broadcaster, [[]]),
+	register(broadcaster, Broadcaster),
+	ok.
 
-stop(Broadcaster) ->
-	Broadcaster ! {stop}.
+stop() ->
+	broadcaster ! {stop},
+	unregister(broadcaster),
+	ok.
